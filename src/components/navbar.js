bbb542e107616308ffc8a1a3c833609b06388a88
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-04-21 11:10:59 -0700

    Update the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navbar">
-            <Link className="links" to="/">Home</Link>
-            <Link className="links" to="/about">About</Link>
+            {/* <Link className="links" to="/">Home</Link> */}
         </div>
     )
 }
